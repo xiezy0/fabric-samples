fe1aaa103b0087396f86a46c80b793b8a760c65b
yin <yin.shen@thoughtworks.com>
2021-02-05 17:36:12 +0800

    fix: adjust config path

--- scripts/ci_scripts/test_sdk.sh
+++ scripts/ci_scripts/test_sdk.sh
@@ -9,10 +9,7 @@ docker run --rm \
     -v "$PWD/${BUILD_DIR}/bin:/opt/gopath/bin" \
     -v "$PWD/${BUILD_DIR}/pkg:/opt/gopath/pkg" \
     -v "$PWD/${BUILD_DIR}/gocache:/opt/gopath/cache" \
-    -v "$PWD/first-network/zhonghuan-ce:/var/zhonghuan" \
     -e GOCACHE=/opt/gopath/cache \
-    -e "ZHONGHUAN_CE_CONFIG" \
-    -e "ZHONGHUAN_CE_ON" \
     -w "$PWD/fabcar" \
     --network net_byfn \
     twblockchain/fabric-baseimage:latest \
